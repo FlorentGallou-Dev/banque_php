5aeb21ce2e2bade9691632ea928ff3b36e8390a5
Florent GALLOU <florentgallou@gmail.com>
2021-05-23 22:49:51 +0200

    DB user added bug repaired final commit

--- banque_php.sql
+++ banque_php.sql
@@ -1,67 +1,70 @@
 DROP DATABASE IF EXISTS banque_php;
-SHOW WARNINGS;
 
 CREATE DATABASE banque_php CHARACTER SET 'utf8';
 
-USE banque_php
+USE banque_php;
 
-CREATE TABLE User (
-    id_user INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_first_name VARCHAR(50) NOT NULL,
-    user_last_name VARCHAR(50) NOT NULL,
-    user_birth_date DATE NOT NULL,
-    user_sex CHAR(1),
-    user_street_number int(4) NULL,
-    user_street_name VARCHAR(255) NOT NULL,
-    user_zip_code VARCHAR(7) NOT NULL,
-    user_city VARCHAR(100) NOT NULL,
-    user_email VARCHAR(150) NOT NULL,
-    user_login VARCHAR(12) NOT NULL,
-    user_passworld VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id_user)
-)
-ENGINE=INNODB;
+DROP USER 'banquePHP'@'localhost';
+FLUSH PRIVILEGES;
 
-INSERT INTO User
+CREATE USER 'banquePHP'@'localhost' IDENTIFIED BY 'gripsec';
+GRANT ALL ON banque_php.* TO 'banquePHP'@'localhost';
+
+CREATE TABLE Customer (
+    id_cust INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cust_first_name VARCHAR(50) NOT NULL,
+    cust_last_name VARCHAR(50) NOT NULL,
+    cust_birth_date DATE NOT NULL,
+    cust_sex CHAR(1),
+    cust_street_number int(4) NULL,
+    cust_street_name VARCHAR(255) NOT NULL,
+    cust_zip_code VARCHAR(7) NOT NULL,
+    cust_city VARCHAR(100) NOT NULL,
+    cust_email VARCHAR(150) NOT NULL,
+    cust_passworld VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_cust)
+) ENGINE=INNODB;
+
+INSERT INTO Customer
 VALUES 
-('Hagrid', 'Rubeus', '1928-12-06' , 'M', NULL, 'Chanteau de Poudlard', 'B2E', 'Loch Lomond', 'rubeus.hagrid@poudlard.uk', 'Hagger', 'Graup');
+(1, 'Hagrid', 'Rubeus', '1928-12-06' , 'M', NULL, 'Chanteau de Poudlard', 'B2E', 'Loch Lomond', 'rubeus.hagrid@poudlard.uk', 'Graup');
 
 CREATE TABLE Account (
     id_acc SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     acc_type VARCHAR(50) NOT NULL,
-    acc_number INT(12) NOT NULL,
+    acc_number INT(25) NOT NULL,
     acc_creation_date DATE NOT NULL,
     acc_balance DECIMAL(12,2) NOT NULL,
-    id_user FOREIGNKEY UNSIGNED NOT NULL,
-    PRIMARY KEY (id_acc)
-)
-ENGINE=INNODB;
+    id_cust INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_acc),
+    CONSTRAINT fk_cust_account FOREIGN KEY (id_cust) REFERENCES Customer(id_cust)
+) ENGINE=INNODB;
 
 INSERT INTO Account
 VALUES 
-('Compte courant', 111222333, '1940-07-01' , 42014869, 1),
-('PEL', 35134456835, '1993-05-08' , 952352154, 1),
-('Livret A', 326584215489, '1942-07-01' , 42, 1);
+(1, 'Compte courant', 111222333, '1940-07-01' , 42014869, 1),
+(2, 'PEL', 35134456835, '1993-05-08' , 952352154, 1),
+(3, 'Livret A', 326584215489, '1942-07-01' , 42, 1);
 
 CREATE TABLE Movement (
     id_move INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    mov_title VARCHAR(150)) NOT NULL,
-    mov_amount DECIMAL(12,2) NOT NULL,
+    mov_title VARCHAR(150) NOT NULL,
+    mov_amount DECIMAL(14,2) NOT NULL,
     mov_date DATETIME NOT NULL,
     mov_type CHAR(1) NOT NULL,
-    id_acc FOREIGNKEY UNSIGNED NOT NULL,
-    PRIMARY KEY (id_move)
-)
-ENGINE=INNODB;
+    id_acc SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_move),
+    CONSTRAINT fk_account_movement FOREIGN KEY (id_acc) REFERENCES Account(id_acc)
+) ENGINE=INNODB;
 
-INSERT INTO Account
+INSERT INTO Movement
 VALUES 
-("Dépot d'ouverture", 560000, '1940-07-01 09:23:00','D', 1),
-("Retrait", -20, '1940-07-24 11:15:00', 'R', 1),
-("Dépot salaire", 523, '1993-06-01 17:00:00', 'D', 1),
-("Retrait moto", -14000, '1993-06-13 15:21:00', 'R', 1),
-("Dépot salaire", 523, '1993-07-01 17:00:00', 'D', 1),
-("Dépot d'ouverture", 420000, '1993-05-08 18:42:00', 'D', 2),
-("Dépot", 300, '1993-08-15 13:04:00', 'D', 2),
-("Dépot", 4625, '1993-12-08 13:14:00', 'D', 2),
-("Dépot d'ouverture", 42, '1942-07-01 09:43:00', 'D', 3);
+(1, "Dépot d'ouverture", 560000, '1940-07-01 09:23:00','D', 1),
+(2, "Retrait", -20, '1940-07-24 11:15:00', 'R', 1),
+(3, "Dépot salaire", 523, '1993-06-01 17:00:00', 'D', 1),
+(4, "Retrait moto", -14000, '1993-06-13 15:21:00', 'R', 1),
+(5, "Dépot salaire", 523, '1993-07-01 17:00:00', 'D', 1),
+(6, "Dépot d'ouverture", 420000, '1993-05-08 18:42:00', 'D', 2),
+(7, "Dépot", 300, '1993-08-15 13:04:00', 'D', 2),
+(8, "Dépot", 4625, '1993-12-08 13:14:00', 'D', 2),
+(9, "Dépot d'ouverture", 42, '1942-07-01 09:43:00', 'D', 3);
